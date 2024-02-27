bf8e15a6e8d17c448e4a1d8ef493602d9334305d
dangchien2003 <chienboy03@gmail.com>
2024-02-27 23:57:44 +0700

    Update configdb.php

--- src/config/configdb.php
+++ src/config/configdb.php
@@ -1,5 +1,5 @@
 <?php
-    function connectDB(){
+    function connectDB_LOCAL(){
         $svn = "localhost";
         $usv = "root";
         $psv = "";
@@ -11,17 +11,31 @@
         }
     }
 
-    // function connectDB(){
-    //     $svn = "sql109.infinityfree.com";
-    //     $usv = "if0_35868743";
-    //     $psv = "chiennkoi123";
-    //     $dbsv = "if0_35868743_qlcuahang";
-    //     $GLOBALS['conn'] = new mysqli($svn, $usv, $psv, $dbsv);
-    //     $GLOBALS['conn']->set_charset("utf8");
-    //     if($GLOBALS['conn']->connect_error) {
-    //         header("location ../page/dangnhap.php?status=400&message=Lỗi server");
-    //     }
-    // }
+    function connectDB() {
+        switch($_SERVER['SERVER_NAME']) {
+            case "nhom5-php.kesug.com": 
+                connectDB_HOSTING();
+                break;
+            case "localhost": 
+                connectDB_LOCAL();
+                break;
+            default:
+                echo "Tên miền chưa xác định";
+                exit();
+        }
+    }
+
+    function connectDB_HOSTING(){
+        $svn = "sql109.infinityfree.com";
+        $usv = "if0_35868743";
+        $psv = "chiennkoi123";
+        $dbsv = "if0_35868743_qlcuahang";
+        $GLOBALS['conn'] = new mysqli($svn, $usv, $psv, $dbsv);
+        $GLOBALS['conn']->set_charset("utf8");
+        if($GLOBALS['conn']->connect_error) {
+            header("location ../page/dangnhap.php?status=400&message=Lỗi server");
+        }
+    }
     function get_key() {
         return "key";
     }
